6b2858a8ed226438f800a7ace6b39ea09d95feab
Joao Victor Nunes <jvmm.nunes@gmail.com>
2023-07-11 17:27:49 -0300

    bugfix: testando .flaskenv vazio

--- config.py
+++ config.py
@@ -16,11 +16,11 @@ class Config(object):
     a boolean flag to enable encrypted connections, 
     and optional username and password
     """
-    # env_values = dotenv_values(".flaskenv")
+    env_values = dotenv_values(".flaskenv")
 
-    # MAIL_SERVER = env_values['MAIL_SERVER']
-    # MAIL_PORT = int(env_values['MAIL_PORT'] or 25)
-    # MAIL_USE_TLS = env_values['MAIL_USE_TLS'] is not None
-    # MAIL_USERNAME = env_values['MAIL_USERNAME']
-    # MAIL_PASSWORD = env_values['MAIL_PASSWORD']
-    # ADMINS = env_values['ADMINS']
+    MAIL_SERVER = env_values['MAIL_SERVER']
+    MAIL_PORT = int(env_values['MAIL_PORT'] or 25)
+    MAIL_USE_TLS = env_values['MAIL_USE_TLS'] is not None
+    MAIL_USERNAME = env_values['MAIL_USERNAME']
+    MAIL_PASSWORD = env_values['MAIL_PASSWORD']
+    ADMINS = env_values['ADMINS']
